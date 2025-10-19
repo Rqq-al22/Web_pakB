8fdbf99f9f0bdb3a6aadaedf1088a9a5015bed91
Rqq-al22 <rezkialya0909@gmail.com>
2025-10-19 20:07:30 +0800

    Update elem-icons.php

--- template/elem-icons.php
+++ template/elem-icons.php
@@ -59,12 +59,12 @@
     <div class="az-header">
       <div class="container">
         <div class="az-header-left">
-          <a href="../strive/index.php" class="az-logo"><span></span> azia</a>
+          <a href="index.php" class="az-logo"><span></span> azia</a>
           <a href="" id="azMenuShow" class="az-header-menu-icon d-lg-none"><span></span></a>
         </div><!-- az-header-left -->
         <div class="az-header-menu">
           <div class="az-header-menu-header">
-            <a href="../strive/index.php" class="az-logo"><span></span> azia</a>
+            <a href="index.php" class="az-logo"><span></span> azia</a>
             <a href="" class="close">&times;</a>
           </div><!-- az-header-menu-header -->
           <ul class="nav">
@@ -243,7 +243,7 @@
           <a href="edit1.php?id=<?php echo $row['IDPetani']; ?>">
             <img src="../img/edit.png" alt="Edit" width="20">
           </a>
-          <a href="delete.php?id=<?php echo $row['IDPetani']; ?>" onclick="return confirm('Yakin ingin menghapus data ini?')">
+          <a href="delete3.php?id=<?php echo $row['IDPetani']; ?>" onclick="return confirm('Yakin ingin menghapus data ini?')">
             <img src="../img/delete.png" alt="Delete" width="25">
           </a>
         </td>
